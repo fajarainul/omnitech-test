a1930b997b0d3234cdde322d5daabe2cf671803e
fajar ainul <fajarainul14@gmail.com>
2020-09-01 10:17:15 +0700

    create page edit profile

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import {
 import LoginPage from './src/screens/LoginPage';
 import MainPage from './src/screens/MainPage';
 import FormNote from './src/screens/FormNote';
+import FormProfile from './src/screens/FormProfile';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -35,28 +36,37 @@ const App: () => React$Node = () => {
           <Stack.Screen
             name="Main"
             component={MainPage}
-            options={{
-              headerLeft : null,
-              title : "Main Page",
-              headerRight: () => (
-                <View
-                  style={{padding : 16}}
-                >
-                  <TouchableOpacity
-                    onPress={()=>alert('click')}
+            options={({ navigation, route }) => (
+              {
+                headerLeft : null,
+                title : "Main Page",
+                headerRight: () => (
+                  <View
+                    style={{padding : 16}}
                   >
-                    <Icon name="user" size={16} color="gray" />
-                  </TouchableOpacity>
-
-                </View>
-              ),
-            }}
+                    <TouchableOpacity
+                      onPress={()=> navigation.navigate('FormProfile')}
+                    >
+                      <Icon name="user" size={16} color="gray" />
+                    </TouchableOpacity>
+  
+                  </View>
+                ),
+              }
+            )}
           />
           <Stack.Screen
             name="Form"
             component={FormNote}
             options={{
               title : "Form Note"
+            }}
+          />
+          <Stack.Screen
+            name="FormProfile"
+            component={FormProfile}
+            options={{
+              title : "Form Profile"
             }}
           />
         </Stack.Navigator>
