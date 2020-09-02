b4e014624418bc06bd37a9877bb54ba8c661e1fe
fajar ainul <fajarainul14@gmail.com>
2020-09-02 11:51:24 +0700

    delete note from db

--- src/screens/MainPage.js
+++ src/screens/MainPage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {
   View,
   TouchableOpacity,
-  FlatList
+  FlatList,
+  Alert
 } from 'react-native';
 
 import NoteItem from '../components/item/note_item'
 import EmptyNote from '../components/empty/empty_note'
 import FloatingActionButton from '../components/button/floating_action_button'
-import {getNote, getNotes} from '../database/NoteDB'
+import {getNotes, deleteNote} from '../database/NoteDB'
   
 class MainPage extends React.Component{
     
@@ -28,9 +29,25 @@ class MainPage extends React.Component{
         })
     }
 
+    confirmDelete = (id) =>
+        Alert.alert(
+        "Delete Note",
+        "Are you sure you want to delete?",
+        [
+            { text: "Yes", onPress: () => deleteNote(id) },
+            {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+            },
+            
+        ],
+        { cancelable: true }
+        );
+
     render(){
         const renderItem = ({ item }) => (
-            <NoteItem title={item.title} desc={item.desc}/>
+            <NoteItem title={item.title} desc={item.desc} onPressDelete={()=>this.confirmDelete(item.id)}/>
           );
 
         return(
